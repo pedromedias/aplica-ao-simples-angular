511c088c774fc3342034a88c342bb31ebf7f158f
Pedro Medeiros <pedrohmd14@gmail.com>
2022-06-12 12:11:11 -0300

    aplicaçao simple angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { ImagensComponent } from './components/imagens/imagens.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { MainComponent } from './components/main/main.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: MainComponent},
+  {path:'sobre', component: SobreComponent},
+  {path:'imagens', component: ImagensComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { ImagensComponent } from './components/imagens/imagens.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    SobreComponent,
+    ImagensComponent
   ],
   imports: [
     BrowserModule,
